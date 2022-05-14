46e679025700104b96e4c7878bf840e5bcdf6096
Ofri Zangi <ofrizangi@gmail.com>
2022-05-14 11:46:46 +0300

    created api project

--- WhatsappProject/Migrations/20220513151943_database-changed.Designer.cs
+++ WhatsappProject/Migrations/20220513151943_database-changed.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WhatsappServer;
 
@@ -10,9 +11,10 @@ using WhatsappServer;
 namespace WhatsappProject.Migrations
 {
     [DbContext(typeof(ItemsContext))]
-    partial class ItemsContextModelSnapshot : ModelSnapshot
+    [Migration("20220513151943_database-changed")]
+    partial class databasechanged
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
