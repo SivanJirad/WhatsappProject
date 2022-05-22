21b32649da11d02e1aa654cc4c889b94863d938b
SivanJhirad <sivanush550@gamil.com>
2022-05-22 20:34:07 +0300

    signalr - working

--- WebWhatsapp/Hubs/ChatHub.cs
+++ WebWhatsapp/Hubs/ChatHub.cs
@@ -2,15 +2,34 @@
 using WebWhatsappApi.Models;
 
 using Microsoft.AspNetCore.SignalR;
+using System.Collections;
 
 namespace WebWhatsappApi.Hubs
 
 {
     public class ChatHub : Hub<IChatClient>
     {
+        private static Hashtable htUsers_ConIds = new Hashtable(20);
+        public void registerConId(string userID)
+        {
+            var key = "user: " + userID;
+            if (htUsers_ConIds.ContainsKey(key))
+                htUsers_ConIds[key] = Context.ConnectionId;
+            else
+                htUsers_ConIds.Add(key, Context.ConnectionId);
+        }
+
         public async Task SendMessage(Transfer message)
         {
-            await Clients.All.ReceiveMessage(message);
+            var key = "user: " + message.To;
+
+            var a = (string)htUsers_ConIds[message.To];
+            await Clients.Client((string)htUsers_ConIds[key]).ReceiveMessage(message);
+
+            //await Clients.All.ReceiveMessage(message);
         }
     }
 }
+
+
+
